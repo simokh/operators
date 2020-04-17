19c4d10ec2f04050ce2f7d549efa551dfc782b4a
simokh <m.simokhouili@gmail.com>
2020-04-16 22:08:00 -0400

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,18 @@
-def unsafe?(speed)
 
+require 'pry'
+def unsafe?(speed) 
+#    if speed.between?(40, 60)
+#     false 
+#    else 
+#     true  
+#    end 
+speed.between?(40, 60) ? false : true 
 end
 
 
 
 def not_safe?(speed)
-	
+speed.between?(40, 60) ? false : true 
 end
 	
 
